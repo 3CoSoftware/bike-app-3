70eca6a7b7faf313a8050c95545747279ff37247
Megan Comeau <mlcomeau99@gmail.com>
2021-05-08 15:40:31 -0700

    move google key

--- src/components/Ride.jsx
+++ src/components/Ride.jsx
@@ -21,7 +21,7 @@ class Ride extends Component {
                 </div>
                 
                 <WrappedMap 
-                    googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyAJDxdV_uSLcEHfaZrIn-G4jyZKmO9NZYo"
+                    googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY}`}
                     loadingElement={<div style={{ height: `100%` }} />}
                     containerElement={<div style={{ height: `400px` }} />}
                     mapElement={<div style={{ height: `100%` }} />}
